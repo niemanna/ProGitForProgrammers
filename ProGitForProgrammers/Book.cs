27393b2820d0e4deedcac643868ca9d134f5d9bd
Andreas Niemann <andreas@niemann-web.de>
2023-01-15 18:05:57 +0100

    Add Book class

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+    }
+}
