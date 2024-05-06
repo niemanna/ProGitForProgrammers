a273804b682cdd231b282fa39ce233255e375f38
Andreas Niemann <andreas@niemann-web.de>
2024-05-06 08:48:15 +0200

    Add another Console command

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -2,4 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
 Console.WriteLine("I just added this in the command line repo");
+Console.WriteLine("This line added to demonstrate messages.");
 Console.WriteLine("This line added to show the staging area");
+
