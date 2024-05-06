09e9c960113821fab66caa76aea59fbd53ffd293
Andreas Niemann <andreas@niemann-web.de>
2024-05-06 09:03:57 +0200

    Merge conflict test

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
 Console.WriteLine("I just added this in the command line repo");
 Console.WriteLine("This line added to show the staging area");
+Console.WriteLine("This line added to test merge conflicts");
